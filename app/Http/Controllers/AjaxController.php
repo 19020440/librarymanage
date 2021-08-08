24709a471a420ba51aa4f6599705f9e3ba409f1a
19020440 <you@example.com>
2021-08-08 21:38:13 +0700

    master

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -98,9 +98,9 @@ class AjaxController extends Controller
         // if ($valueRequest != 0) return  $this->bookRepository->searchDataAjax($valueRequest);
         // else return  response()
         //     ->json([]);
-        // if ($valueRequest != 0) 
+        if ($valueRequest != 0) 
         return  Book::select($valueRequest) ->distinct()->get();
-        // else return $valueRequest;
+        else return $valueRequest;
         // return $valueRequest;
     }
 
